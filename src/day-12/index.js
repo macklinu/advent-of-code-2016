2e38b8855f2ab480f8786e6c614b49f0dbb94627
Macklin Underdown <macklinu@gmail.com>
2017-12-29 18:47:57 -0500

    Solve Day 12 Part 2

--- src/day-12/index.js
+++ src/day-12/index.js
@@ -1,23 +1,24 @@
 import { split, filter, map } from 'lodash/fp'
 
-export default input => {
+export default (registerOverride = {}) => input => {
+  let toValue = x => register => (Number.isNaN(+x) ? register[x] || 0 : +x)
+  let instructionToFn = {
+    cpy: (x, y) => register => (register[y] = toValue(x)(register)),
+    inc: x => register => (register[x] += 1),
+    dec: x => register => (register[x] -= 1),
+    jnz: (x, y) => register => ((toValue(x)(register) || 0) === 0 ? 1 : +y),
+  }
+
   let instructions = input |> split('\n') |> filter(Boolean) |> map(split(' '))
   let index = 0
-  let registers = { a: 0, b: 0, c: 0, d: 0 }
+  let registers = { a: 0, b: 0, c: 0, d: 0, ...registerOverride }
+
   while (index < instructions.length) {
     let [i, ...args] = instructions[index]
     let update = instructionToFn[i].apply(null, args)
     let result = update(registers)
     index += i === 'jnz' ? result : 1
   }
+
   return registers
-}
-
-let toValue = x => register => (Number.isNaN(+x) ? register[x] || 0 : +x)
-
-let instructionToFn = {
-  cpy: (x, y) => register => (register[y] = toValue(x)(register)),
-  inc: x => register => (register[x] += 1),
-  dec: x => register => (register[x] -= 1),
-  jnz: (x, y) => register => ((toValue(x)(register) || 0) === 0 ? 1 : +y),
 }
--- src/day-12/index.test.js
+++ src/day-12/index.test.js
@@ -10,8 +10,12 @@ jnz a 2
 dec a`
 
 test('solves part 1', () => {
-  let registerA = input => input |> executeProgram |> get('a')
-
+  let registerA = input => input |> executeProgram() |> get('a')
   expect(registerA(exampleInput)).toBe(42)
   expect(registerA(puzzleInput)).toBe(318009)
+})
+
+test('solves part 2', () => {
+  let registerA = input => input |> executeProgram({ c: 1 }) |> get('a')
+  expect(registerA(puzzleInput)).toBe(9227663)
 })
