ede7ac7b9491af9552d68d99a17b035a05605260
Macklin Underdown <macklinu@gmail.com>
2017-12-29 13:14:59 -0500

    Delete unused fp functions

--- src/@fp/previous.js
+++
@@ -1,7 +0,0 @@
-export function previous(array = []) {
-  return function(value) {
-    const index = array.indexOf(value)
-    if (index === -1) return null
-    return array[index - 1] || array[array.length + index - 1]
-  }
-}
