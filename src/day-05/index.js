ec3fcf922ecf6e4d17e1b224cf6f3c27d85cfdd8
Macklin Underdown <macklinu@gmail.com>
2017-12-31 13:29:10 -0500

    Solve Day 5 Part 2

--- src/day-05/index.js
+++ src/day-05/index.js
@@ -1,12 +1,28 @@
 import md5 from 'md5'
 
-export default input => {
+export let partOne = input => {
   let arr = []
   let i = 0
   while (arr.length < 8) {
     let hash = md5(input + i++)
     if (hash.slice(0, 5) === '00000') {
       arr.push(hash[5])
+    }
+  }
+  return arr.join('')
+}
+
+export let partTwo = input => {
+  let arr = Array(8).fill(undefined)
+  let i = 0
+  while (arr.some(v => v == null)) {
+    let hash = md5(input + i++)
+    if (hash.slice(0, 5) === '00000') {
+      let position = hash[5]
+      let value = hash[6]
+      if (position < 8 && arr[position] === undefined) {
+        arr[position] = value
+      }
     }
   }
   return arr.join('')
--- src/day-05/index.test.js
+++ src/day-05/index.test.js
@@ -1,6 +1,13 @@
-import password from './'
+import { partOne, partTwo } from './'
 
-test('solves part 1', () => {
-  // expect(password('abc')).toBe('18f47a30')
-  expect(password('ffykfhsq')).toBe('c6697b55')
+// Tests take a while to run
+test.skip('solves part 1', () => {
+  expect(partOne('abc')).toBe('18f47a30')
+  expect(partOne('ffykfhsq')).toBe('c6697b55')
+})
+
+// Tests take a while to run
+test.skip('solves part 2', () => {
+  expect(partTwo('abc')).toBe('05ace8e3')
+  expect(partTwo('ffykfhsq')).toBe('8c35d1ab')
 })
